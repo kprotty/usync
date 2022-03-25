13b6c71b91cf427c510c9b2b2736989e03de4a59
kprotty <kbutcher6200@gmail.com>
2022-03-25 10:12:56 -0500

    raw mutex impl

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 // mod barrier;
 // mod condvar;
 // mod fair_mutex;
-// mod mutex;
+mod mutex;
 // mod once;
 // mod reentrant_mutex;
 mod rwlock;
@@ -26,7 +26,7 @@ pub use self::{
     // barrier::{Barrier, BarrierWaitResult},
     // condvar::{Condvar, WaitTimeoutResult},
     // fair_mutex::{const_fair_mutex, FairMutex, FairMutexGuard, MappedFairMutexGuard, RawFairMutex},
-    // mutex::{const_mutex, MappedMutexGuard, Mutex, MutexGuard, RawMutex},
+    mutex::{const_mutex, MappedMutexGuard, Mutex, MutexGuard, RawMutex},
     // once::{Once, OnceState},
     // reentrant_mutex::{
     //     const_reentrant_mutex, MappedReentrantMutexGuard, ReentrantMutex, ReentrantMutexGuard,
--- src/mutex.rs
+++ src/mutex.rs
@@ -1,3 +1,40 @@
+use super::RawRwLock;
+use lock_api::RawRwLock as _RawRwLock;
+
+#[derive(Default)]
+#[repr(transparent)]
+pub struct RawMutex {
+    rwlock: RawRwLock,
+}
+
+unsafe impl lock_api::RawMutex for RawMutex {
+    type GuardMarker = crate::GuardMarker;
+
+    const INIT: Self = Self {
+        rwlock: RawRwLock::INIT,
+    };
+
+    #[inline]
+    fn is_locked(&self) -> bool {
+        self.rwlock.is_locked_exclusive()
+    }
+
+    #[inline]
+    fn lock(&self) {
+        self.rwlock.lock_exclusive()
+    }
+
+    #[inline]
+    fn try_lock(&self) -> bool {
+        self.rwlock.try_lock_exclusive()
+    }
+
+    #[inline]
+    unsafe fn unlock(&self) {
+        self.rwlock.unlock_exclusive()
+    }
+}
+
 pub type Mutex<T> = lock_api::Mutex<RawMutex, T>;
 pub type MutexGuard<'a, T> = lock_api::MutexGuard<'a, RawMutex, T>;
 pub type MappedMutexGuard<'a, T> = lock_api::MappedMutexGuard<'a, RawMutex, T>;
