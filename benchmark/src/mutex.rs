ea6f12a1b1c149ef88d1a801276892da89003f9d
kprotty <kbutcher6200@gmail.com>
2022-03-28 12:35:06 -0500

    mutex and rwlock benchmarks

---
+++ benchmark/src/mutex.rs
@@ -0,0 +1,333 @@
+// Copyright 2016 Amanieu d'Antras
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+mod args;
+use crate::args::ArgRange;
+
+#[cfg(any(windows, unix))]
+use std::cell::UnsafeCell;
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc, Barrier,
+    },
+    thread,
+    time::Duration,
+};
+
+trait Mutex<T> {
+    fn new(v: T) -> Self;
+    fn lock<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R;
+    fn name() -> &'static str;
+}
+
+impl<T> Mutex<T> for std::sync::Mutex<T> {
+    fn new(v: T) -> Self {
+        Self::new(v)
+    }
+    fn lock<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        f(&mut *self.lock().unwrap())
+    }
+    fn name() -> &'static str {
+        "std::sync::Mutex"
+    }
+}
+
+impl<T> Mutex<T> for usync::Mutex<T> {
+    fn new(v: T) -> Self {
+        Self::new(v)
+    }
+    fn lock<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        f(&mut *self.lock())
+    }
+    fn name() -> &'static str {
+        "usync::Mutex"
+    }
+}
+
+impl<T> Mutex<T> for parking_lot::Mutex<T> {
+    fn new(v: T) -> Self {
+        Self::new(v)
+    }
+    fn lock<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        f(&mut *self.lock())
+    }
+    fn name() -> &'static str {
+        "parking_lot::Mutex"
+    }
+}
+
+#[cfg(not(windows))]
+type SrwLock<T> = std::sync::Mutex<T>;
+
+#[cfg(windows)]
+use winapi::um::synchapi;
+#[cfg(windows)]
+struct SrwLock<T>(UnsafeCell<T>, UnsafeCell<synchapi::SRWLOCK>);
+#[cfg(windows)]
+unsafe impl<T> Sync for SrwLock<T> {}
+#[cfg(windows)]
+unsafe impl<T: Send> Send for SrwLock<T> {}
+#[cfg(windows)]
+impl<T> Mutex<T> for SrwLock<T> {
+    fn new(v: T) -> Self {
+        let mut h: synchapi::SRWLOCK = synchapi::SRWLOCK {
+            Ptr: std::ptr::null_mut(),
+        };
+
+        unsafe {
+            synchapi::InitializeSRWLock(&mut h);
+        }
+        SrwLock(UnsafeCell::new(v), UnsafeCell::new(h))
+    }
+    fn lock<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        unsafe {
+            synchapi::AcquireSRWLockExclusive(self.1.get());
+            let res = f(&mut *self.0.get());
+            synchapi::ReleaseSRWLockExclusive(self.1.get());
+            res
+        }
+    }
+    fn name() -> &'static str {
+        "SRWLOCK"
+    }
+}
+
+#[cfg(not(unix))]
+type PthreadMutex<T> = std::sync::Mutex<T>;
+
+#[cfg(unix)]
+struct PthreadMutex<T>(UnsafeCell<T>, UnsafeCell<libc::pthread_mutex_t>);
+#[cfg(unix)]
+unsafe impl<T> Sync for PthreadMutex<T> {}
+#[cfg(unix)]
+impl<T> Mutex<T> for PthreadMutex<T> {
+    fn new(v: T) -> Self {
+        PthreadMutex(
+            UnsafeCell::new(v),
+            UnsafeCell::new(libc::PTHREAD_MUTEX_INITIALIZER),
+        )
+    }
+    fn lock<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        unsafe {
+            libc::pthread_mutex_lock(self.1.get());
+            let res = f(&mut *self.0.get());
+            libc::pthread_mutex_unlock(self.1.get());
+            res
+        }
+    }
+    fn name() -> &'static str {
+        "pthread_mutex_t"
+    }
+}
+#[cfg(unix)]
+impl<T> Drop for PthreadMutex<T> {
+    fn drop(&mut self) {
+        unsafe {
+            libc::pthread_mutex_destroy(self.1.get());
+        }
+    }
+}
+
+fn run_benchmark<M: Mutex<f64> + Send + Sync + 'static>(
+    num_threads: usize,
+    work_per_critical_section: usize,
+    work_between_critical_sections: usize,
+    seconds_per_test: usize,
+) -> Vec<usize> {
+    let lock = Arc::new(([0u8; 300], M::new(0.0), [0u8; 300]));
+    let keep_going = Arc::new(AtomicBool::new(true));
+    let barrier = Arc::new(Barrier::new(num_threads));
+    let mut threads = vec![];
+    for _ in 0..num_threads {
+        let barrier = barrier.clone();
+        let lock = lock.clone();
+        let keep_going = keep_going.clone();
+        threads.push(thread::spawn(move || {
+            let mut local_value = 0.0;
+            let mut value = 0.0;
+            let mut iterations = 0usize;
+            barrier.wait();
+            while keep_going.load(Ordering::Relaxed) {
+                lock.1.lock(|shared_value| {
+                    for _ in 0..work_per_critical_section {
+                        *shared_value += value;
+                        *shared_value *= 1.01;
+                        value = *shared_value;
+                    }
+                });
+                for _ in 0..work_between_critical_sections {
+                    local_value += value;
+                    local_value *= 1.01;
+                    value = local_value;
+                }
+                iterations += 1;
+            }
+            (iterations, value)
+        }));
+    }
+
+    thread::sleep(Duration::from_secs(seconds_per_test as u64));
+    keep_going.store(false, Ordering::Relaxed);
+    threads.into_iter().map(|x| x.join().unwrap().0).collect()
+}
+
+fn run_benchmark_iterations<M: Mutex<f64> + Send + Sync + 'static>(
+    num_threads: usize,
+    work_per_critical_section: usize,
+    work_between_critical_sections: usize,
+    seconds_per_test: usize,
+    test_iterations: usize,
+) {
+    let mut data = vec![];
+    for _ in 0..test_iterations {
+        let run_data = run_benchmark::<M>(
+            num_threads,
+            work_per_critical_section,
+            work_between_critical_sections,
+            seconds_per_test,
+        );
+        data.extend_from_slice(&run_data);
+    }
+
+    let average = data.iter().fold(0f64, |a, b| a + *b as f64) / data.len() as f64;
+    let variance = data
+        .iter()
+        .fold(0f64, |a, b| a + ((*b as f64 - average).powi(2)))
+        / data.len() as f64;
+    data.sort();
+
+    let k_hz = 1.0 / seconds_per_test as f64 / 1000.0;
+    println!(
+        "{:20} | {:10.3} kHz | {:10.3} kHz | {:10.3} kHz",
+        M::name(),
+        average * k_hz,
+        data[data.len() / 2] as f64 * k_hz,
+        variance.sqrt() * k_hz
+    );
+}
+
+fn run_all(
+    args: &[ArgRange],
+    first: &mut bool,
+    num_threads: usize,
+    work_per_critical_section: usize,
+    work_between_critical_sections: usize,
+    seconds_per_test: usize,
+    test_iterations: usize,
+) {
+    if num_threads == 0 {
+        return;
+    }
+    if *first || !args[0].is_single() {
+        println!("- Running with {} threads", num_threads);
+    }
+    if *first || !args[1].is_single() || !args[2].is_single() {
+        println!(
+            "- {} iterations inside lock, {} iterations outside lock",
+            work_per_critical_section, work_between_critical_sections
+        );
+    }
+    if *first || !args[3].is_single() {
+        println!("- {} seconds per test", seconds_per_test);
+    }
+    *first = false;
+
+    println!(
+        "{:^20} | {:^14} | {:^14} | {:^14}",
+        "name", "average", "median", "std.dev."
+    );
+
+    run_benchmark_iterations::<usync::Mutex<f64>>(
+        num_threads,
+        work_per_critical_section,
+        work_between_critical_sections,
+        seconds_per_test,
+        test_iterations,
+    );
+
+    run_benchmark_iterations::<parking_lot::Mutex<f64>>(
+        num_threads,
+        work_per_critical_section,
+        work_between_critical_sections,
+        seconds_per_test,
+        test_iterations,
+    );
+
+    run_benchmark_iterations::<std::sync::Mutex<f64>>(
+        num_threads,
+        work_per_critical_section,
+        work_between_critical_sections,
+        seconds_per_test,
+        test_iterations,
+    );
+    if cfg!(windows) {
+        run_benchmark_iterations::<SrwLock<f64>>(
+            num_threads,
+            work_per_critical_section,
+            work_between_critical_sections,
+            seconds_per_test,
+            test_iterations,
+        );
+    }
+    if cfg!(unix) {
+        run_benchmark_iterations::<PthreadMutex<f64>>(
+            num_threads,
+            work_per_critical_section,
+            work_between_critical_sections,
+            seconds_per_test,
+            test_iterations,
+        );
+    }
+}
+
+fn main() {
+    let args = args::parse(&[
+        "numThreads",
+        "workPerCriticalSection",
+        "workBetweenCriticalSections",
+        "secondsPerTest",
+        "testIterations",
+    ]);
+    let mut first = true;
+    for num_threads in args[0] {
+        for work_per_critical_section in args[1] {
+            for work_between_critical_sections in args[2] {
+                for seconds_per_test in args[3] {
+                    for test_iterations in args[4] {
+                        run_all(
+                            &args,
+                            &mut first,
+                            num_threads,
+                            work_per_critical_section,
+                            work_between_critical_sections,
+                            seconds_per_test,
+                            test_iterations,
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ benchmark/src/rwlock.rs
@@ -0,0 +1,411 @@
+// Copyright 2016 Amanieu d'Antras
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+mod args;
+use crate::args::ArgRange;
+
+#[cfg(any(windows, unix))]
+use std::cell::UnsafeCell;
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc, Barrier,
+    },
+    thread,
+    time::Duration,
+};
+
+trait RwLock<T> {
+    fn new(v: T) -> Self;
+    fn read<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&T) -> R;
+    fn write<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R;
+    fn name() -> &'static str;
+}
+
+impl<T> RwLock<T> for std::sync::RwLock<T> {
+    fn new(v: T) -> Self {
+        Self::new(v)
+    }
+    fn read<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&T) -> R,
+    {
+        f(&*self.read().unwrap())
+    }
+    fn write<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        f(&mut *self.write().unwrap())
+    }
+    fn name() -> &'static str {
+        "std::sync::RwLock"
+    }
+}
+
+impl<T> RwLock<T> for usync::RwLock<T> {
+    fn new(v: T) -> Self {
+        Self::new(v)
+    }
+    fn read<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&T) -> R,
+    {
+        f(&*self.read())
+    }
+    fn write<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        f(&mut *self.write())
+    }
+    fn name() -> &'static str {
+        "usync::RwLock"
+    }
+}
+
+impl<T> RwLock<T> for parking_lot::RwLock<T> {
+    fn new(v: T) -> Self {
+        Self::new(v)
+    }
+    fn read<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&T) -> R,
+    {
+        f(&*self.read())
+    }
+    fn write<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        f(&mut *self.write())
+    }
+    fn name() -> &'static str {
+        "parking_lot::RwLock"
+    }
+}
+
+#[cfg(not(windows))]
+#[allow(unused)]
+type SrwLock<T> = std::sync::RwLock<T>;
+
+#[cfg(windows)]
+use winapi::um::synchapi;
+#[cfg(windows)]
+struct SrwLock<T>(UnsafeCell<T>, UnsafeCell<synchapi::SRWLOCK>);
+#[cfg(windows)]
+unsafe impl<T> Sync for SrwLock<T> {}
+#[cfg(windows)]
+unsafe impl<T: Send> Send for SrwLock<T> {}
+#[cfg(windows)]
+impl<T> RwLock<T> for SrwLock<T> {
+    fn new(v: T) -> Self {
+        let mut h: synchapi::SRWLOCK = synchapi::SRWLOCK {
+            Ptr: std::ptr::null_mut(),
+        };
+
+        unsafe {
+            synchapi::InitializeSRWLock(&mut h);
+        }
+        SrwLock(UnsafeCell::new(v), UnsafeCell::new(h))
+    }
+    fn read<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&T) -> R,
+    {
+        unsafe {
+            synchapi::AcquireSRWLockShared(self.1.get());
+            let res = f(&*self.0.get());
+            synchapi::ReleaseSRWLockShared(self.1.get());
+            res
+        }
+    }
+    fn write<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        unsafe {
+            synchapi::AcquireSRWLockExclusive(self.1.get());
+            let res = f(&mut *self.0.get());
+            synchapi::ReleaseSRWLockExclusive(self.1.get());
+            res
+        }
+    }
+    fn name() -> &'static str {
+        "SRWLOCK"
+    }
+}
+
+#[cfg(not(unix))]
+type PthreadRwLock<T> = std::sync::RwLock<T>;
+
+#[cfg(unix)]
+struct PthreadRwLock<T>(UnsafeCell<T>, UnsafeCell<libc::pthread_rwlock_t>);
+#[cfg(unix)]
+unsafe impl<T> Sync for PthreadRwLock<T> {}
+#[cfg(unix)]
+impl<T> RwLock<T> for PthreadRwLock<T> {
+    fn new(v: T) -> Self {
+        PthreadRwLock(
+            UnsafeCell::new(v),
+            UnsafeCell::new(libc::PTHREAD_RWLOCK_INITIALIZER),
+        )
+    }
+    fn read<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&T) -> R,
+    {
+        unsafe {
+            libc::pthread_rwlock_wrlock(self.1.get());
+            let res = f(&*self.0.get());
+            libc::pthread_rwlock_unlock(self.1.get());
+            res
+        }
+    }
+    fn write<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        unsafe {
+            libc::pthread_rwlock_wrlock(self.1.get());
+            let res = f(&mut *self.0.get());
+            libc::pthread_rwlock_unlock(self.1.get());
+            res
+        }
+    }
+    fn name() -> &'static str {
+        "pthread_rwlock_t"
+    }
+}
+#[cfg(unix)]
+impl<T> Drop for PthreadRwLock<T> {
+    fn drop(&mut self) {
+        unsafe {
+            libc::pthread_rwlock_destroy(self.1.get());
+        }
+    }
+}
+
+fn run_benchmark<M: RwLock<f64> + Send + Sync + 'static>(
+    num_writer_threads: usize,
+    num_reader_threads: usize,
+    work_per_critical_section: usize,
+    work_between_critical_sections: usize,
+    seconds_per_test: usize,
+) -> (Vec<usize>, Vec<usize>) {
+    let lock = Arc::new(([0u8; 300], M::new(0.0), [0u8; 300]));
+    let keep_going = Arc::new(AtomicBool::new(true));
+    let barrier = Arc::new(Barrier::new(num_reader_threads + num_writer_threads));
+    let mut writers = vec![];
+    let mut readers = vec![];
+    for _ in 0..num_writer_threads {
+        let barrier = barrier.clone();
+        let lock = lock.clone();
+        let keep_going = keep_going.clone();
+        writers.push(thread::spawn(move || {
+            let mut local_value = 0.0;
+            let mut value = 0.0;
+            let mut iterations = 0usize;
+            barrier.wait();
+            while keep_going.load(Ordering::Relaxed) {
+                lock.1.write(|shared_value| {
+                    for _ in 0..work_per_critical_section {
+                        *shared_value += value;
+                        *shared_value *= 1.01;
+                        value = *shared_value;
+                    }
+                });
+                for _ in 0..work_between_critical_sections {
+                    local_value += value;
+                    local_value *= 1.01;
+                    value = local_value;
+                }
+                iterations += 1;
+            }
+            (iterations, value)
+        }));
+    }
+    for _ in 0..num_reader_threads {
+        let barrier = barrier.clone();
+        let lock = lock.clone();
+        let keep_going = keep_going.clone();
+        readers.push(thread::spawn(move || {
+            let mut local_value = 0.0;
+            let mut value = 0.0;
+            let mut iterations = 0usize;
+            barrier.wait();
+            while keep_going.load(Ordering::Relaxed) {
+                lock.1.read(|shared_value| {
+                    for _ in 0..work_per_critical_section {
+                        local_value += value;
+                        local_value *= *shared_value;
+                        value = local_value;
+                    }
+                });
+                for _ in 0..work_between_critical_sections {
+                    local_value += value;
+                    local_value *= 1.01;
+                    value = local_value;
+                }
+                iterations += 1;
+            }
+            (iterations, value)
+        }));
+    }
+
+    thread::sleep(Duration::new(seconds_per_test as u64, 0));
+    keep_going.store(false, Ordering::Relaxed);
+
+    let run_writers = writers
+        .into_iter()
+        .map(|x| x.join().unwrap().0)
+        .collect::<Vec<usize>>();
+    let run_readers = readers
+        .into_iter()
+        .map(|x| x.join().unwrap().0)
+        .collect::<Vec<usize>>();
+
+    (run_writers, run_readers)
+}
+
+fn run_benchmark_iterations<M: RwLock<f64> + Send + Sync + 'static>(
+    num_writer_threads: usize,
+    num_reader_threads: usize,
+    work_per_critical_section: usize,
+    work_between_critical_sections: usize,
+    seconds_per_test: usize,
+    test_iterations: usize,
+) {
+    let mut writers = vec![];
+    let mut readers = vec![];
+
+    for _ in 0..test_iterations {
+        let (run_writers, run_readers) = run_benchmark::<M>(
+            num_writer_threads,
+            num_reader_threads,
+            work_per_critical_section,
+            work_between_critical_sections,
+            seconds_per_test,
+        );
+        writers.extend_from_slice(&run_writers);
+        readers.extend_from_slice(&run_readers);
+    }
+
+    let total_writers = writers.iter().fold(0f64, |a, b| a + *b as f64) / test_iterations as f64;
+    let total_readers = readers.iter().fold(0f64, |a, b| a + *b as f64) / test_iterations as f64;
+    println!(
+        "{:20} - [write] {:10.3} kHz [read] {:10.3} kHz",
+        M::name(),
+        total_writers as f64 / seconds_per_test as f64 / 1000.0,
+        total_readers as f64 / seconds_per_test as f64 / 1000.0
+    );
+}
+
+fn run_all(
+    args: &[ArgRange],
+    first: &mut bool,
+    num_writer_threads: usize,
+    num_reader_threads: usize,
+    work_per_critical_section: usize,
+    work_between_critical_sections: usize,
+    seconds_per_test: usize,
+    test_iterations: usize,
+) {
+    if num_writer_threads == 0 && num_reader_threads == 0 {
+        return;
+    }
+    if *first || !args[0].is_single() || !args[1].is_single() {
+        println!(
+            "- Running with {} writer threads and {} reader threads",
+            num_writer_threads, num_reader_threads
+        );
+    }
+    if *first || !args[2].is_single() || !args[3].is_single() {
+        println!(
+            "- {} iterations inside lock, {} iterations outside lock",
+            work_per_critical_section, work_between_critical_sections
+        );
+    }
+    if *first || !args[4].is_single() {
+        println!("- {} seconds per test", seconds_per_test);
+    }
+    *first = false;
+
+    run_benchmark_iterations::<usync::RwLock<f64>>(
+        num_writer_threads,
+        num_reader_threads,
+        work_per_critical_section,
+        work_between_critical_sections,
+        seconds_per_test,
+        test_iterations,
+    );
+    run_benchmark_iterations::<parking_lot::RwLock<f64>>(
+        num_writer_threads,
+        num_reader_threads,
+        work_per_critical_section,
+        work_between_critical_sections,
+        seconds_per_test,
+        test_iterations,
+    );
+    if cfg!(windows) {
+        run_benchmark_iterations::<std::sync::RwLock<f64>>(
+            num_writer_threads,
+            num_reader_threads,
+            work_per_critical_section,
+            work_between_critical_sections,
+            seconds_per_test,
+            test_iterations,
+        );
+    }
+    if cfg!(unix) {
+        run_benchmark_iterations::<PthreadRwLock<f64>>(
+            num_writer_threads,
+            num_reader_threads,
+            work_per_critical_section,
+            work_between_critical_sections,
+            seconds_per_test,
+            test_iterations,
+        );
+    }
+}
+fn main() {
+    let args = args::parse(&[
+        "numWriterThreads",
+        "numReaderThreads",
+        "workPerCriticalSection",
+        "workBetweenCriticalSections",
+        "secondsPerTest",
+        "testIterations",
+    ]);
+    let mut first = true;
+    for num_writer_threads in args[0] {
+        for num_reader_threads in args[1] {
+            for work_per_critical_section in args[2] {
+                for work_between_critical_sections in args[3] {
+                    for seconds_per_test in args[4] {
+                        for test_iterations in args[5] {
+                            run_all(
+                                &args,
+                                &mut first,
+                                num_writer_threads,
+                                num_reader_threads,
+                                work_per_critical_section,
+                                work_between_critical_sections,
+                                seconds_per_test,
+                                test_iterations,
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
